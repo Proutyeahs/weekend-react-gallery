f8cbd388f3900fd04ae7df5d508898e958450007
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-12 16:31:42 -0500

    save

--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,18 @@
 import './GalleryItem.css'
+import axios from 'axios'
+import { response } from 'express'
 
 function GalleryItem({image}) {
+
+    const like = () => {
+        axios({
+            method: 'PUT',
+            url: `/like/${image.id}`
+        }).then( (response) => {
+            
+        })
+    }
+// .map needs to be in GalleryList
     return (
         <>
         <p>Items</p>
@@ -9,6 +21,7 @@ function GalleryItem({image}) {
                 (<span key={picture.id}><img src={picture.path} height={100} width={100}/></span>)
             )}
         </ul>
+        <button>Like!</button>
         </>
         
     )
