9816548ebbc59ef7b1c5e6ff66437c88e5649ce0
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-14 17:21:26 -0500

    added read me and blocked empty inputs

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # Project Name
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+React Photo Gallery
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+Duration: August 12th - 14th
+
+This app allows you to input photos to a gallery, add likes and remove the ones your done with.  When clicking on the image a description will pop up in its place with the details that were input!
+
+This app was built with:
+React.js
+Node.js
+JSX
+SQL
+Express.js
+Axios
+HTML
+CSS
+PG
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- src/components/App/GalleryForm/GalleryForm.jsx
+++ src/components/App/GalleryForm/GalleryForm.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react';
 import './GalleryForm.css'
 
-function GalleryForm({newImage}) {
+function GalleryForm({ newImage }) {
 
     const [img, setImg] = useState('')
     const [description, setDescription] = useState('')
 
+
     const submit = () => {
-        newImage(img, description)
-        setDescription('')
-        setImg('')
+        if (img != "") {
+            newImage(img, description)
+            setDescription('')
+            setImg('')
+        } else {
+            alert('Please submit and image link!')
+        }
     }
 
     return (
         <>
             <h3>Add new Image:</h3>
-            <input className="input" type="text" placeholder="Image link" onChange={(e) => setImg(e.target.value)} value={img}/>
-            <input className="input" type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)} value={description}/>
+            <input className="input" type="text" placeholder="Image link" onChange={(e) => setImg(e.target.value)} value={img} />
+            <input className="input" type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)} value={description} />
             <p><button className="hover" onClick={submit}>Submit</button></p>
         </>
     )
