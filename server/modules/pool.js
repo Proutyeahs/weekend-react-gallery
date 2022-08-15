59e7ca1009876597de8e9458e7c25302803a170f
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-15 12:24:44 -0500

    setting up heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,20 +2,42 @@ const pg = require('pg');
 
 const Pool = pg.Pool;
 
-const pool = new Pool({
-    database: 'react_gallery',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-})
+const url = require('url');
 
-pool.on('connect', () => {
-    console.log('Connected to database')
-})
+let config = {};
 
-pool.on('error', () => {
-    comsole.log('Error cconnecting to database')
-})
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
 
-module.exports = pool
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: { rejectUnauthorized: false },
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+} else {
+    const pool = new Pool({
+        database: 'react_gallery',
+        host: 'localhost',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000
+    })
+}
+
+    pool.on('connect', () => {
+        console.log('Connected to database')
+    })
+
+    pool.on('error', () => {
+        comsole.log('Error cconnecting to database')
+    })
+
+    module.exports = pool
